651ccc6f0a88bb50310bc85286cd17b2df571837
Arjun Thekkumadathil 2 <t.arjun@tai3d.com>
2016-08-12 17:41:43 +0530

    redifined UI and added more details, added asset task info by which refresh n open is little slow, settask method also complete

--- README.md
+++ README.md
@@ -11,6 +11,13 @@
 * `export $PIPEDEV='E:/pipeline/root/dir'`
 * `export $PYTHONPATH=$PYTHONPATH:'E:/pipeline/root/dir'`
 
+## Important Environment Variables in a project context
+* Assets
+`PROJECT`: Picks up the project context from this environment variable
+`ASSET`: Currently active asset is known from this environment variable
+`TASKSTEP`: Currently active department / process of the asset / shot will be 
+known from this variable like (model/rig/lighting/fx etc.)
+
 ## Overview 
 
 This is quick and simple pipeline based on simple directory structure and simple database
--- core/assetManager.py
+++ core/assetManager.py
@@ -1,17 +1,19 @@
 import os
 import getpass
 import json
+import datetime
 from sgstudio import sgframework
 from PySide import QtGui
 from PySide import QtCore
+from core import sandbox
 
 SERVER_PATH = "https://pipetest.shotgunstudio.com"
 SCRIPT_NAME = 'info'
 SCRIPT_KEY = '90aea59877cebbb6445bf7e77f5d90a9663ff0ba45d5eebae168703a6ad98cd2'
 
-class MovieTreeWidget(QtGui.QTreeWidget):
+class CustomTreeWidget(QtGui.QTreeWidget):
     def __init__(self,parent):
-        super(MovieTreeWidget, self).__init__(parent)
+        super(CustomTreeWidget, self).__init__(parent)
 
     def addToTree(self, treeList):
         '''
@@ -25,45 +27,64 @@ class UserInterface(QtGui.QWidget):
     def __init__(self):
         super(UserInterface, self).__init__()
         self.setGeometry(300, 300, 400, 120)
-        self.setMinimumSize(950,620) 
+        self.setMinimumSize(900,520) 
         self.setWindowTitle('Asset Manager')
 
-        # self.projects = QtGui.QComboBox()
-        # self.projects.addItem("Select Projects")
-        self.taskLister = MovieTreeWidget(self)
-        self.taskLister.setColumnCount(5)
-        self.taskLister.setHeaderLabels(['Task Name', 'Asset', 'Status',
-            'Start Date', 'Due Date'])
+        groupBox = QtGui.QGroupBox("Information")
+        namefonts = QtGui.QFont()
+        namefonts.setBold(True)
+        namefonts.setPointSize(10)
 
-        self.taskVersion = MovieTreeWidget(self)
+        currentAssetLabel = QtGui.QLabel("Current Asset: ")
+        self.currentAssetNameLabel = QtGui.QLabel(str(os.environ['ASSETNAME']))
+        self.currentAssetNameLabel.setFont(namefonts)
+        currentProjectLabel = QtGui.QLabel("Current Project: ")
+        self.currentProjectNameLabel = QtGui.QLabel(str(os.environ['PROJECT']))
+        self.currentProjectNameLabel.setFont(namefonts)
+        currentDeptLabel = QtGui.QLabel("Current Department: ")
+        self.currentDeptNameLabel = QtGui.QLabel(str(os.environ['TASKSTEP']))
+        self.currentDeptNameLabel.setFont(namefonts)
+
+        self.taskLister = CustomTreeWidget(self)
+        self.taskLister.setSortingEnabled(True)
+        self.taskLister.setColumnCount(5)
+        self.taskLister.setHeaderLabels(['Task Name', 'Asset', 'Type', 
+            'Status', 'Start Date', 'Due Date', 'bid', 'Department', 
+            'Time Remaining as of Today'])
+
+        self.taskVersion = CustomTreeWidget(self)
+        self.taskVersion.setSortingEnabled(True)
         self.taskVersion.setColumnCount(3)
         self.taskVersion.setHeaderLabels(['Version', 'Date', 'Author', 'Note'])
 
         self.progressbar = QtGui.QProgressBar()        
 
         self.refreshButton = QtGui.QPushButton('Refresh')
-        self.submitButton = QtGui.QPushButton('Check Out')
+        self.checkoutButton = QtGui.QPushButton('Check Out')
+        self.checkinButton = QtGui.QPushButton('Check In')
+        self.settaskButton = QtGui.QPushButton('Set Task')
 
         vbox = QtGui.QVBoxLayout(self)
-        # projectLayout = QtGui.QHBoxLayout()
-        # tagLayout = QtGui.QHBoxLayout()
+        infoLabelLayout = QtGui.QHBoxLayout()
         tasklayout = QtGui.QVBoxLayout()
         buttonLayout = QtGui.QHBoxLayout()
 
-        # projectLayout.addWidget(self.projects)
+        infoLabelLayout.addWidget(currentProjectLabel)
+        infoLabelLayout.addWidget(self.currentProjectNameLabel)
+        infoLabelLayout.addWidget(currentAssetLabel)
+        infoLabelLayout.addWidget(self.currentAssetNameLabel)
+        infoLabelLayout.addWidget(currentDeptLabel)
+        infoLabelLayout.addWidget(self.currentDeptNameLabel)
+
         tasklayout.addWidget(self.taskLister)
         tasklayout.addWidget(self.taskVersion)
 
-        # tagLayout.addWidget(self.renderFinal)
-        # tagLayout.addWidget(self.errorRender)
-        # tagLayout.addWidget(self.missingFrames)
-        # tagLayout.addWidget(self.inputNotReady)
-        # tasklayout.addLayout(tagLayout)
-
-        # tasklayout.addWidget(self.commentBox)
+        buttonLayout.addWidget(self.checkinButton)
         buttonLayout.addWidget(self.refreshButton)
-        buttonLayout.addWidget(self.submitButton)
-        # vbox.addLayout(projectLayout)
+        buttonLayout.addWidget(self.settaskButton)
+        buttonLayout.addWidget(self.checkoutButton)
+        groupBox.setLayout(infoLabelLayout)
+        vbox.addWidget(groupBox)
         vbox.addLayout(tasklayout)
         vbox.addWidget(self.progressbar)
         vbox.addLayout(buttonLayout)
@@ -78,10 +99,32 @@ class AssetManager(UserInterface):
     def __init__(self):
         super(AssetManager, self).__init__()
         self.sgf = sgframework.SgFrameWork(SERVER_PATH, SCRIPT_NAME, SCRIPT_KEY)
-        self.statusJson = json.load(open(os.path.join(os.getenv('PIPEDEV'), 'config/statuses.json')))
+        self.statusJson = json.load(open(os.path.join(os.getenv('PIPEDEV'), 
+            'config/statuses.json')))
         self.loadUserTasks()
 
         self.refreshButton.clicked.connect(self.loadUserTasks)
+        self.settaskButton.clicked.connect(self.setTaskSettings)
+
+    def setTaskSettings(self):
+        currentSelection = self.taskLister.selectedItems()[0]
+        os.environ["ASSETNAME"] = currentSelection.text(1)
+        os.environ["TASKSTEP"] = currentSelection.text(7)
+        os.environ["ASSETTYPE"] = currentSelection.text(2)
+        self.currentAssetNameLabel.setText(str(currentSelection.text(1)))
+        self.currentDeptNameLabel.setText(str(currentSelection.text(6)))
+        sbox = sandbox.SandBox()
+        currentAssetSandBox = sbox.getSandBox()
+        if currentAssetSandBox is None:
+            sbox.create()
+
+    def remainingTime(self, dueDate):
+        currentDate = datetime.date.today()
+        dateSplit = dueDate.split("-")
+        dueDateObj = datetime.date(int(dateSplit[0]), int(dateSplit[1]), 
+            int(dateSplit[2]))
+        remainingDays = dueDateObj - currentDate
+        return str(remainingDays).split(',')[0]
 
     def loadUserTasks(self):
         self.taskLister.clear()
@@ -93,6 +136,10 @@ class AssetManager(UserInterface):
         self.progressbar.setRange(0,len(allTasks))
         for index, task in enumerate(allTasks):
             self.progressbar.setValue(index+1)
+            assetObject = sgframework.Asset(**task.entity)
             if task.sg_status_list not in self.statusJson['approved']:
-                self.taskLister.addToTree([task.content, task.entity['name'], 
-                    task.sg_status_list, task.start_date, task.due_date])
+                self.taskLister.addToTree([task.content, task.entity['name'],
+                assetObject.sg_asset_type, task.sg_status_list, 
+                task.start_date, task.due_date,
+                "{0} Days".format(task.duration/600), task.step['name'],
+                    self.remainingTime(task.due_date)])
