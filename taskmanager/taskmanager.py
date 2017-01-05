e3573892e8779cb57649836c74973593802c1169
ARJUN THEKKUMADATHIL <aes10233@epica.studio.com>
2017-01-05 19:05:05 +0530

    changed db structure and started adding interaction to taskmanager UI

--- taskmanager/taskmanager.py
+++ taskmanager/taskmanager.py
@@ -9,6 +9,18 @@ __email__ = "arjun.thekkumadathil@gmail.com"
 class TaskManager(taskmanagerui.UserInterface):
     def __init__(self):
         super(TaskManager, self).__init__()
+        self.assetTaskType.clicked.connect(self.assetShotSelection)
+        self.shotTaskType.clicked.connect(self.assetShotSelection)
+
+    def assetShotSelection(self):
+        if self.assetTaskType.isChecked():
+            self.taskSeqName.setEnabled(False)
+            self.taskShotName.setEnabled(False)
+            self.taskAssetName.setEnabled(True)
+        elif self.shotTaskType.isChecked():
+            self.taskAssetName.setEnabled(False)
+            self.taskSeqName.setEnabled(True)
+            self.taskShotName.setEnabled(True)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
